7fd08761aaef63152dc265fb0a6454e1f0f82f99
banmoon <1678591193@qq.com>
2023-02-21 10:06:19 +0800

    Site updated: 2023-02-21 10:06:12

--- js/banmoon.js
+++ js/banmoon.js
@@ -1,3 +1,23 @@
 'use strict'
 
-console.log(GLOBAL_CONFIG_SITE);
+function getId(id){
+    return document.getElementById(id);
+};
+
+function copy(e) {
+    console.log(e);
+    e.select();
+    e.setSelectionRange(0, e.value.length);
+    document.execCommand('copy');
+    alert("已复制到剪切板");
+}
+
+function insertMaven () {
+    let $g = getId('g');
+    let $a = getId('a');
+    let $v = getId('v');
+    let $f = getId('f');
+    let $result = getId('result');
+    let result = `mvn install:install-file -DgroupId=${$g.value} -DartifactId=${$a.value} -Dversion=${$v.value} -Dpackaging=jar -Dfile=${$f.value}`
+    $result.value = result;
+}
