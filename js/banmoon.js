e8f8a278f1c2be9caeaf7056d194b0153c69835c
吴昊埕 <ban-moon@outlook.com>
2023-08-31 11:40:05 +0800

    Site updated: 2023-08-31 11:39:55

--- js/banmoon.js
+++ js/banmoon.js
@@ -1,18 +1,17 @@
 'use strict'
 
-function getId(id){
+function getId(id) {
     return document.getElementById(id);
 };
 
 function copy(e) {
-    console.log(e);
     e.select();
     e.setSelectionRange(0, e.value.length);
     document.execCommand('copy');
     alert("已复制到剪切板");
 }
 
-function insertMaven () {
+function insertMaven() {
     let $g = getId('g');
     let $a = getId('a');
     let $v = getId('v');
@@ -22,6 +21,29 @@ function insertMaven () {
     $result.value = result;
 }
 
-// getId("card-info-btn").onclick = function () {
-//     btf.snackbarShow("点击 ctrl + D 加入书签");
-// }
+window.onload = function () {
+    // 处理内部a标签跳转动画问题，但还没有解决
+    const $alink = document.querySelectorAll('a[href^="#"]');
+    if ($alink) {
+        for (let i = 0; i < $alink.length; i++) {
+            const alink = $alink[i];
+            if (!alink.classList.contains('toc-link')) {
+                alink.addEventListener('click', e => {
+                    const $target = document.getElementById(decodeURI(e.target.getAttribute('href')).replace('#', ''));
+                    btf.scrollToDest(btf.getEleTop($target), 300)
+                    if (window.innerWidth < 900) {
+                        window.mobileToc.close()
+                    }
+                })
+            }
+        }
+    }
+
+    // 首页按钮点击
+    const $cardBtn = document.querySelector('#card-info-btn');
+    if ($cardBtn) {
+        $cardBtn.addEventListener('click', e => {
+            btf.snackbarShow("点击 ctrl + D 加入书签");
+        })
+    }
+}
